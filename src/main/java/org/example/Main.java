8b096aca3707d7cc36e9aba063a255c0991f34db
nouran3abdelaal <nourakhaled329@gmail.com>
2023-10-03 14:39:36 +0300

    Problem with tests testWithdrawFromMyAccount, testWithdrawFromMyAccountNotAcceptableAmount

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,33 +9,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws NotValidMailException {
-//        userController control = new userController();
-//        control.login();
+        userController control = new userController();
+        control.login();
         //
-        userController userController = new userController();
-        List<user> userList = new ArrayList<>();
-        userController.setUserList(userList);
-        account account1 = new account("Savings", 1000.0);
-        account account2 = new account("Current", 500.0);
-        Set<account> accountSet = new HashSet<>();
-        accountSet.add(account1);
-        accountSet.add(account2);
-        user user1 = new user("user1", "t1@gmail.com", "pass1", accountSet);
-        account1.setAccountOwnerID(user1.getID());
-        account2.setAccountOwnerID(user1.getID());
-        System.out.println(user1);
-        userController.withdrawService(user1);
-        System.out.println(user1);
-        Optional<account> account = user1.getAccountset().stream().filter(a -> (a.getID() + "").equals("5")).findAny();
-        if (account.isPresent()) {
-            account account3 = account.get();
-            System.out.println("yes account");
-
-            System.out.println(account3.toString());
-        }
-        else {
-            System.out.println("no account");
-        }
+//        userController userController = new userController();
+//        List<user> userList = new ArrayList<>();
+//        userController.setUserList(userList);
+//        account account1 = new account("Savings", 1000.0);
+//        account account2 = new account("Current", 500.0);
+//        Set<account> accountSet = new HashSet<>();
+//        accountSet.add(account1);
+//        accountSet.add(account2);
+//        user user1 = new user("user1", "t1@gmail.com", "pass1", accountSet);
+//        account1.setAccountOwnerID(user1.getID());
+//        account2.setAccountOwnerID(user1.getID());
+//        System.out.println(user1);
+//        userController.withdrawService(user1);
+//        System.out.println(user1);
+//        Optional<account> account = user1.getAccountset().stream().filter(a -> (a.getID() + "").equals("5")).findAny();
+//        if (account.isPresent()) {
+//            account account3 = account.get();
+//            System.out.println("yes account");
+//
+//            System.out.println(account3.toString());
+//        }
+//        else {
+//            System.out.println("no account");
+//        }
         //
 
 
